1b57da85ba9952ae340c4284dc68e6e7ef6e8b83
Sean Porter <portertech@gmail.com>
2012-11-07 11:15:22 -0800

    create a lwrp for instruments

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default.librato_metrics.email = "example@domain.com"
+default.librato_metrics.token = "secret"
---
+++ providers/instrument.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: librato-metrics
+# Provider:: instrument
+#
+# Copyright 2012, Sean Porter Consulting
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+action :create do
+  email = new_resource.email || node.librato_metrics.email
+  token = new_resource.token || node.librato_metrics.token
+  librato = Librato::Metrics.new(email, token)
+  librato.create_instrument(new_resource.name, new_resource.streams)
+end
