de0d7547586fcb82335da37b8fcb2d221071888b
Sean Porter <portertech@gmail.com>
2012-12-12 22:04:21 -0800

    [dashboard] cookbook version bump

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "portertech@gmail.com"
 license          "Apache 2.0"
 description      "Provides LWRPs to manage Librato Metrics."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
